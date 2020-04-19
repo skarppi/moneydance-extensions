6777f6db4e3eb4025198bacdb79015a2658fa571
Juho Kolehmainen <juho.kolehmainen@futurice.com>
2020-04-19 09:15:23 +0300

    Copy example files from moneydance devkit 5.1

---
+++ src/com/moneydance/modules/features/salaryslip/AccountListWindow.java
@@ -0,0 +1,122 @@
+/************************************************************\
+ *      Copyright (C) 2016 The Infinite Kind, Limited       *
+\************************************************************/
+
+package com.moneydance.modules.features.salaryslip;
+
+import com.moneydance.awt.*;
+import com.infinitekind.moneydance.model.*;
+
+import java.io.*;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.border.*;
+
+/** Window used for Account List interface */
+
+public class AccountListWindow 
+  extends JFrame
+  implements ActionListener
+{
+  private Main extension;
+  private JTextArea accountListArea;
+  private JButton clearButton;
+  private JButton closeButton;
+  private JTextField inputArea;
+
+  public AccountListWindow(Main extension) {
+    super("Account List Console");
+    this.extension = extension;
+
+    accountListArea = new JTextArea();
+    
+    AccountBook book = extension.getUnprotectedContext().getCurrentAccountBook();
+    StringBuffer acctStr = new StringBuffer();
+    if(book !=null) {
+      addSubAccounts(book.getRootAccount(), acctStr);
+    }
+    accountListArea.setEditable(false);
+    accountListArea.setText(acctStr.toString());
+    inputArea = new JTextField();
+    inputArea.setEditable(true);
+    clearButton = new JButton("Clear");
+    closeButton = new JButton("Close");
+
+    JPanel p = new JPanel(new GridBagLayout());
+    p.setBorder(new EmptyBorder(10,10,10,10));
+    p.add(new JScrollPane(accountListArea), AwtUtil.getConstraints(0,0,1,1,4,1,true,true));
+    p.add(Box.createVerticalStrut(8), AwtUtil.getConstraints(0,2,0,0,1,1,false,false));
+    p.add(clearButton, AwtUtil.getConstraints(0,3,1,0,1,1,false,true));
+    p.add(closeButton, AwtUtil.getConstraints(1,3,1,0,1,1,false,true));
+    getContentPane().add(p);
+
+    setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+    enableEvents(WindowEvent.WINDOW_CLOSING);
+    closeButton.addActionListener(this);
+    clearButton.addActionListener(this);
+        
+    PrintStream c = new PrintStream(new ConsoleStream());
+
+    setSize(500, 400);
+    AwtUtil.centerWindow(this);
+  }
+
+  public static void addSubAccounts(Account parentAcct, StringBuffer acctStr) {
+    int sz = parentAcct.getSubAccountCount();
+    for(int i=0; i<sz; i++) {
+      Account acct = parentAcct.getSubAccount(i);
+      acctStr.append(acct.getFullAccountName());
+      acctStr.append("\n");
+      addSubAccounts(acct, acctStr);
+    }
+  }
+
+
+  public void actionPerformed(ActionEvent evt) {
+    Object src = evt.getSource();
+    if(src==closeButton) {
+      extension.closeConsole();
+    }
+    if(src==clearButton) {
+      accountListArea.setText("");
+    }
+  }
+
+  public final void processEvent(AWTEvent evt) {
+    if(evt.getID()==WindowEvent.WINDOW_CLOSING) {
+      extension.closeConsole();
+      return;
+    }
+    if(evt.getID()==WindowEvent.WINDOW_OPENED) {
+    }
+    super.processEvent(evt);
+  }
+  
+  private class ConsoleStream
+    extends OutputStream
+    implements Runnable
+  {    
+    public void write(int b)
+      throws IOException
+    {
+      accountListArea.append(String.valueOf((char)b));
+      repaint();
+    }
+
+    public void write(byte[] b)
+      throws IOException
+    {
+      accountListArea.append(new String(b));
+      repaint();
+    }
+    public void run() {
+      accountListArea.repaint();
+    }
+  }
+
+  void goAway() {
+    setVisible(false);
+    dispose();
+  }
+}
---
+++ src/com/moneydance/modules/features/salaryslip/Main.java
@@ -0,0 +1,111 @@
+/************************************************************\
+ *      Copyright (C) 2016 The Infinite Kind, Limited       *
+\************************************************************/
+
+package com.moneydance.modules.features.salaryslip;
+
+import com.moneydance.apps.md.controller.FeatureModule;
+import com.moneydance.apps.md.controller.FeatureModuleContext;
+import com.moneydance.apps.md.controller.ModuleUtil;
+import com.moneydance.apps.md.controller.UserPreferences;
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.awt.*;
+
+/** Pluggable module used to give users access to a Account List
+    interface to Moneydance.
+*/
+
+public class Main
+  extends FeatureModule
+{
+  private AccountListWindow accountListWindow = null;
+
+  public void init() {
+    // the first thing we will do is register this module to be invoked
+    // via the application toolbar
+    FeatureModuleContext context = getContext();
+    try {
+      context.registerFeature(this, "showconsole",
+        getIcon("accountlist"),
+        getName());
+    }
+    catch (Exception e) {
+      e.printStackTrace(System.err);
+    }
+  }
+
+  public void cleanup() {
+    closeConsole();
+  }
+  
+  private Image getIcon(String action) {
+    try {
+      ClassLoader cl = getClass().getClassLoader();
+      java.io.InputStream in = 
+        cl.getResourceAsStream("/com/moneydance/modules/features/salaryslip/icon.gif");
+      if (in != null) {
+        ByteArrayOutputStream bout = new ByteArrayOutputStream(1000);
+        byte buf[] = new byte[256];
+        int n = 0;
+        while((n=in.read(buf, 0, buf.length))>=0)
+          bout.write(buf, 0, n);
+        return Toolkit.getDefaultToolkit().createImage(bout.toByteArray());
+      }
+    } catch (Throwable e) { }
+    return null;
+  }
+  
+  /** Process an invokation of this module with the given URI */
+  public void invoke(String uri) {
+    String command = uri;
+    String parameters = "";
+    int theIdx = uri.indexOf('?');
+    if(theIdx>=0) {
+      command = uri.substring(0, theIdx);
+      parameters = uri.substring(theIdx+1);
+    }
+    else {
+      theIdx = uri.indexOf(':');
+      if(theIdx>=0) {
+        command = uri.substring(0, theIdx);
+      }
+    }
+
+    if(command.equals("showconsole")) {
+      showConsole();
+    }    
+  }
+
+  public String getName() {
+    return "Account List";
+  }
+
+  private synchronized void showConsole() {
+    if(accountListWindow==null) {
+      accountListWindow = new AccountListWindow(this);
+      accountListWindow.setVisible(true);
+    }
+    else {
+      accountListWindow.setVisible(true);
+      accountListWindow.toFront();
+      accountListWindow.requestFocus();
+    }
+  }
+  
+  FeatureModuleContext getUnprotectedContext() {
+    return getContext();
+  }
+
+  synchronized void closeConsole() {
+    if(accountListWindow!=null) {
+      accountListWindow.goAway();
+      accountListWindow = null;
+      System.gc();
+    }
+  }
+}
+
+
